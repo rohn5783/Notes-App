38477f55630f767483eca8cdcf9eebf9a9e2ed77
Rohit Pandey <rp5120523@gmail.com>
2026-03-11 02:59:53 +0530

    backend ready for deploy

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,17 @@
 import app from "./src/app.js";
 import redis from "./config/cache.js";
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+// connect redis
+redis.on("connect", () => {
+  console.log("Redis connected successfully");
+});
+
+redis.on("error", (err) => {
+  console.error("Redis connection error:", err);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
