7a7acd210d69769a34096fda2d3442523a72762b
Rohit Pandey <rp5120523@gmail.com>
2026-03-11 08:51:59 +0530

    link changed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -14,7 +14,7 @@ connectDB();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://notes-app-swcq-9frml9vph-rohn5783s-projects.vercel.app/"
+    "https://notes-app-swcq.vercel.app/"
   ],
   credentials: true
 }));
