8007ee17f2f9a75de8c0f3ab71da9c19b6fba98b
Rohit Pandey <rp5120523@gmail.com>
2026-03-12 16:13:42 +0530

    update production API URL

--- frontend/src/services/note.api.js
+++ frontend/src/services/note.api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
  const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://notes-app-2-0d09.onrender.com',
     withCredentials: true,
  })
 
