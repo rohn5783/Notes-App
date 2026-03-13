2b6fd9f52b563b1bbdea5d93e65f3407258d3e7c
Rohit Pandey <rp5120523@gmail.com>
2026-03-13 16:26:55 +0530

    path changed

--- frontend/src/auth/auth.context.jsx
+++ frontend/src/auth/auth.context.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState, useEffect } from "react";
-import { getMe } from "../api/auth.api";
+import { getMe } from "../services/auth.api";
 
 export const AuthContext = createContext();
 
